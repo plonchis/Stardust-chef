783ad8394724d1c439475eb8f292b8e281f35390
Ivan Tello Aguilar <ipus2003@gmail.com>
2017-10-14 15:17:18 -0500

    Lightworld and camera fixes

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
-local baton = require 'baton'
-local gamera = require 'gamera'
+local Baton = require 'baton'
+local Camera = require 'camera'
 local LightWorld = require "light"
 
 -- Controls --
@@ -8,10 +8,12 @@ local controls = {
   right = {'sc:d', 'axis:leftx+', 'button:dpright'},
   up = {'sc:w', 'axis:lefty-', 'button:dpup'},
   down = {'sc:s', 'axis:lefty+', 'button:dpdown'},
-  primary = {'sc:e', 'mouse:1', 'button:a'},
-  secondary = {'sc:f', 'mouse:1', 'button:x'},
+  primary = {'sc:f', 'mouse:1', 'button:a'},
+  secondary = {'sc:g', 'mouse:1', 'button:x'},
+  zoomin = {'sc:q', 'axis:triggerleft'},
+  zoomout = {'sc:e', 'axis:triggereight'}
 }
-input = baton.new(controls, love.joystick.getJoysticks()[1])
+input = Baton.new(controls, love.joystick.getJoysticks()[1])
 
 -- Math functions --
 function math.dist(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
@@ -21,9 +23,9 @@ function math.coords(x,y, dist,angle) return x - math.cos(angle) * dist, y - mat
 
 -- Load --
 function love.load()
+  love.window.setMode(800, 600, {resizable=true, minwidth=400, minheight=300})
   love.physics.setMeter(64) -- 1m = 64px
   world = love.physics.newWorld(0, 0, true)
-  camera = gamera.new(0, 0, 2000, 2000)
 	lightWorld = LightWorld({
     ambient = {150,150,150},
     refractionStrength = 32.0,
@@ -33,7 +35,7 @@ function love.load()
   love.mouse.setCursor(cursor)
   -- Player
   player = {x = 0, y = 0, speed = 2000}
-  player.body = love.physics.newBody(world, 650/2, 650/2, "dynamic")
+  player.body = love.physics.newBody(world, 0, 0, "dynamic")
   player.body:setLinearDamping(player.speed/100)
   player.shape = love.physics.newCircleShape(20)
   player.fixture = love.physics.newFixture(player.body, player.shape, 1)
@@ -51,20 +53,32 @@ function love.load()
   pointer.light:setGlowStrength(0.5)
 
   -- Create shadow bodys
-  player.shadow = lightWorld:newImage(player.img, player.x, player.y)
+  player.shadow = lightWorld:newImage(player.img,0,0)
+  player.shadow:setNormalMap(player.shadow:generateNormalMap(1))
 
+  -- Camera
+  cam = Camera.new()
 end
 
 function love.update(dt)
   love.window.setTitle("Stardust chef (FPS:" .. love.timer.getFPS() .. ")")
-
   world:update(dt)
   input:update()
+  window = {x = love.graphics.getWidth(), y = love.graphics.getHeight()}
+
+  -- Player
+  local movementX = (input:get 'right' - input:get 'left')*player.speed
+  local movementY = (input:get 'down' - input:get 'up')*player.speed
+  player.body:applyForce(movementX, movementY)
+  player.x, player.y = player.body:getPosition()
+  player.shadow:setPosition(player.x, player.y)
+  -- Camera
+  cam:lookAt(player.x, player.y)
+  cam:zoomTo(cam.scale+(input:get 'zoomout' - input:get 'zoomin')*dt/10)
 
 -- Pointer
 
   local mouse = {x = love.mouse.getX(), y = love.mouse.getY()}
-  local window = {x = love.graphics.getWidth(), y = love.graphics.getHeight()}
   mouse.distance = math.dist(mouse.x, mouse.y, window.x/2, window.y/2)
   pointer.distance = math.clamp(0, mouse.distance, math.min(window.y, window.x)/2)
   pointer.angle = math.angle(mouse.x,mouse.y,window.x/2,window.y/2)
@@ -74,25 +88,17 @@ function love.update(dt)
     love.mouse.setVisible(true)
   else love.mouse.setVisible(false) end
 
--- Player
-  local movementX = (input:get 'right' - input:get 'left')*player.speed
-  local movementY = (input:get 'down' - input:get 'up')*player.speed
-  player.x, player.y = player.body:getPosition()
-  player.body:applyForce(movementX, movementY)
-  player.shadow:setPosition(player.x, player.y)
-
-  camera:setPosition(player.x, player.y)
   lightWorld:update(dt)
-  --lightWorld:setTranslation(player.x, player.y, 1)
+--lightWorld:setTranslation(-player.x+window.x/2, -player.y+window.y/2, cam.scale)
 end
 
 function love.draw()
-  camera:draw(function(l,t,w,h)
+  cam:attach()
     lightWorld:draw(function()
       love.graphics.setColor(255, 255, 255)
-      love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+      love.graphics.rectangle("fill", 0, 0, 2000, 2000)
       love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, player.width / 2, player.height / 2)
       love.graphics.draw(pointer.img, pointer.x, pointer.y, 0, 0.5, 0.5, pointer.width / 2, pointer.height / 2)
     end)
-  end)
+  cam:detach()
 end
